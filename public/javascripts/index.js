958a19dea69168e3d45f85ef0f52d2f0902549d0
Bonventure Willis <osoroBonventureWillis@gmail.com>
2023-10-18 14:32:40 +0300

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -93,11 +93,10 @@ function onResults(results) {
       }
       // Extract the portion of the image corresponding to the bounding box from the canvas
 const handImageData = canvasCtx.getImageData(boundingBox.x, boundingBox.y, boundingBox.width, boundingBox.height);
-loadModel(handImageData,boundingBox)
        drawBoundingBox(canvasCtx, boundingBox, { color: '#FF0000', lineWidth: 1.5 });
 
       drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#00FF00', lineWidth: 1 });
-
+l     oadModel(handImageData,boundingBox)
       // Draw landmarks with reduced size
       for (const landmark of landmarks) {
         const x = landmark.x * canvasElement.width;
